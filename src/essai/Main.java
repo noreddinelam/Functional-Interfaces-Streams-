656168a254307e732c5872f45e7b9470de631301
Nouredine <in_lamamra@esi.dz>
2020-02-20 00:03:10 +0100

    apply two FUNCTION INTERFACE

--- src/essai/Main.java
+++ src/essai/Main.java
@@ -7,10 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Function<Etudiant, String> takeName = (e)->{return e.getNom();};
+		Function<String,Character> takeChar =(s)->{return s.charAt(0);};
 		Etudiant a = new Etudiant("sari");
-		System.out.println(takeName.apply(a));
-		
-		
+		System.out.println(takeChar.apply(takeName.apply(a)));	
 	}
 
 }
